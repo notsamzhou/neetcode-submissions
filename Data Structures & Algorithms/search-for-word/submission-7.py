0665a35817ae3e654837f25530ea5a0ea363ad56
Samuel Zhou <45342784+notsamzhou@users.noreply.github.com>
2026-08-08 00:08:48 -0700

    Add: search-for-word - submission-7

---
+++ Data Structures & Algorithms/search-for-word/submission-7.py
@@ -0,0 +1,28 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+
+        def dfs(r, c, idx):
+            if not (0 <= r < len(board)) or not (0 <= c < len(board[0])) or board[r][c] == '\\':
+                return False
+
+            if word[idx] != board[r][c]:
+                return False
+
+            if idx == len(word) - 1:
+                return True
+
+            board[r][c] = '\\'
+
+            res = dfs(r - 1, c, idx + 1) or dfs(r + 1, c, idx + 1) or dfs(r, c - 1, idx + 1) or dfs(r, c + 1, idx + 1)
+
+            board[r][c] = word[idx]
+            return res
+
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if dfs(r, c, 0):
+                    return True
+
+
+        return False
+        
