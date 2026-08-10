abb512d1ebd19936bfc9d0f38f90d46154576a46
Samuel Zhou <45342784+notsamzhou@users.noreply.github.com>
2026-08-09 20:42:51 -0700

    Add: course-schedule-ii - submission-4

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-4.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+
+        graph = defaultdict(list)
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+
+
+        visiting = set()
+        visited = set()
+        res = []
+        def dfs(curr):
+            if curr in visited:
+                return True
+
+            if curr in visiting:
+                return False
+
+            visiting.add(curr)
+
+            for nei in graph[curr]:
+                if not dfs(nei):
+                    return False
+
+            visiting.remove(curr)
+            visited.add(curr)
+            res.append(curr)
+            return True
+
+        
+        for course in range(numCourses):
+            if not dfs(course):
+                return []
+        
+
+        return res[::-1]
