513d5410d3c9b0bb9bfa168d2d16ee45609f6763
Samuel Zhou <45342784+notsamzhou@users.noreply.github.com>
2026-08-12 16:59:48 -0700

    Add: dijkstra - submission-0

---
+++ Data Structures & Algorithms/dijkstra/submission-0.py
@@ -0,0 +1,36 @@
+class Solution:
+    def shortestPath(self, n: int, edges: List[List[int]], src: int) -> Dict[int, int]:
+
+        adj = {}
+        for u, v, cost in edges:
+            if u not in adj:
+                adj[u] = []
+
+            adj[u].append((cost, v))
+
+        dist = [float('inf') for _ in range(n)]
+        dist[src] = 0
+        heap = [(0, src)]
+        visited = set()
+
+        while heap:
+            final, u = heapq.heappop(heap)
+
+            if u not in visited:
+                visited.add(u)
+                dist[u] = final
+
+                if u in adj:
+
+                    for cost, v in adj[u]:
+                        if dist[u] + cost < dist[v]:
+                            heapq.heappush(heap, (dist[u] + cost, v))
+
+        res = {}
+        for i in range(n):
+            res[i] = dist[i] if dist[i] != float('inf') else -1
+
+        return res
+
+
+
