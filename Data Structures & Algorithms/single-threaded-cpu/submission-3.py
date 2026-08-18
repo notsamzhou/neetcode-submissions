c96eab359250b02c9beffac808be92c64e7776a5
Samuel Zhou <45342784+notsamzhou@users.noreply.github.com>
2026-08-18 15:11:46 -0700

    Add: single-threaded-cpu - submission-3

---
+++ Data Structures & Algorithms/single-threaded-cpu/submission-3.py
@@ -0,0 +1,34 @@
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+
+        processes = []
+        for i, task in enumerate(tasks):
+            processes.append((i, task[0], task[1]))
+
+        #processes[i] = task Index, enqueueTime, processingTime
+
+        processes.sort(key = lambda x: (x[1], x[2], x[0]))
+        pq = [(processes[0][2], processes[0][0])]
+        #pq = processingTime, task Index
+
+        res = []
+        i = 1
+        curr_time = processes[0][1]
+        while pq:
+
+            processingTime, index = heapq.heappop(pq)
+            res.append(index)
+            curr_time += processingTime
+
+
+            while i < len(processes) and processes[i][1] <= curr_time:
+
+                heapq.heappush(pq, (processes[i][2], processes[i][0]))
+                i += 1
+
+            if not pq and i < len(processes):
+                curr_time = processes[i][1]
+                heapq.heappush(pq, (processes[i][2], processes[i][0]))
+                i += 1
+
+        return res
